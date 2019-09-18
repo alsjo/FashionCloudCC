6cf7ab6d4f890ce4117982b6f5d2251ec932b5f9
Vitalii <vlprimary@gmail.com>
2019-09-18 11:43:24 +0300

    1st commit

---
+++ FashionCloudCC/FashionCloudCC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FashionCloudCC
+//
+//  Created by vitalii on 9/18/19.
+//  Copyright © 2019 Vitalii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
